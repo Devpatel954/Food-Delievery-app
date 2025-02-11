f07492f8254152c1977587dfecac12c4260fb855
Dev Patel <devpatel@Mac.ht.home>
2025-02-11 02:50:51 -0600

    committed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
         <div className="header-contents">
             <h2>Order your tasty food here</h2>
             <p>
-            Savor the flavors of your favorite dishes, delivered fresh to your doorstep. Indulge in a seamless dining experience with every bite, where convenience meets culinary delight. Your cravings, our priority—delivered with care, speed, and perfection.
+               Savor the flavors of your favorite dishes, delivered fresh to your doorstep. Indulge in a seamless dining experience with every bite, where convenience meets culinary delight. Your cravings, our priority—delivered with care, speed, and perfection.
             </p>
-            <button className='btnn'>View Menu</button>
+            <button>View Menu</button>
         </div>
     </div>
   );
